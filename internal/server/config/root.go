01bd1704fe3263b8eede7b6147eba583178106d6
Ahnaf Tahmid <tahmidtahmid31@gmail.com>
2025-02-24 15:32:35 +0600

    refactor: domain driver structure to function based structure

--- internal/server/config/root.go
+++
@@ -1,8 +0,0 @@
-package config
-
-type Config struct {
-	// Host is the hostname to listen on.
-	Host string `json:"host"`
-	// Port is the port to listen on.
-	Port string `json:"port"`
-}
--- internal/server/new.go
+++ internal/server/new.go
@@ -1,17 +1,42 @@
 // Copyright (C) 2025 Nikash Labs
-// 
+//
 // This file is part of hishab.
-// 
+//
 // hishab is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // hishab is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with hishab.  If not, see <https://www.gnu.org/licenses/>.
 
+package server
+
+import (
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+func NewServer(
+	logger *zap.Logger,
+	config *Config,
+) http.Handler {
+	mux := http.NewServeMux()
+	addRoutes(
+		mux,
+		logger,
+		config,
+	)
+
+	var handler http.Handler = mux
+	// handler = someMiddleware(handler)
+	// handler = someMiddleware2(handler)
+	// handler = someMiddleware3(handler)
+	return handler
+}
